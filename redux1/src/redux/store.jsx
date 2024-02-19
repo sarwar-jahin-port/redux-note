70c09f7d824f9ce70b7b219ac6bb658353d1fe19
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2024-02-19 05:30:09 -0800

    project basic pre code added

--- redux1/src/redux/store.jsx
+++ redux1/src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../redux/features/counter/counterSlice'
+import logger from './middlewares/Logger'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) 
 })
